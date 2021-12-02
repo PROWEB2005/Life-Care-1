6a5c2baf4c6ab19f4587c34f50f05f182ed1e34a
CODER-2305 <Misterhayrulllo@gmail.com>
2021-12-02 21:31:28 +0500

    life

---
+++ pages/admin.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kirish</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+<body>
+    <div class="container">
+        <form action="../components/login-admin.php" method="post">
+            <div class="form-floating">
+                <label for="floatingInput">Email manzili *</label>
+                <input name="email" type="email" class="form-control" id="floatingInput" placeholder="ism@namuna.com" required>
+              </div>
+              <div class="form-floating">
+                  <label for="floatingPassword">Parol *</label>
+                <input name="password" type="password" class="form-control" id="floatingPassword" placeholder="Parol" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Kirish</button>
+       </form>
+    </div>
+    <script src="../js/admin.js"></script>
+</body>
+</html>
