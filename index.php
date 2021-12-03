3804d3a38fb26163b7c0c5c4abff6bff8417ef21
Suyunbek <uzcointg@gmail.com>
2021-12-03 06:50:25 -0800

    git commit -m

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ var_dump($path);
 if ($pages[$path]) {
     include_once("./page/$path.php");
 }
-// else {
-//     include_once("./page/404.php");
-// }
+else {
+    include_once("./page/404.php");
+}
 ?>
